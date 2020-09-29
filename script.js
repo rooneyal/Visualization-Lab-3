774e8c89e17c49be51273d4799114a4d8e940044
rooneyal <rooneyal@bc.edu>
2020-09-29 16:27:57 -0400

    commit

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 d3.csv('cities.csv', d3.autoType).then(data=>{
 
+  console.log('cities', data);
+
   //filter data to European cities only
-  data = data.filter(d=>d.eu === TRUE);
+  data = data.filter(d=>d.eu === true);
 
   //sort the data by population size
   data.sort((a,b)=>b.population-a.population);
 
+
+
   //Add Labels to top 10 cities
   //data = data.slice(0,10);
+//rank>10 use opacity 1 
 
   const extent = d3.extent(data, d=>d.population);
   
   //create simple plot
-  d3.select('body').append('svg')
+  const svg = d3.select('.population-plot').append('svg')
     .attr('width', 500)
     .attr('height', 500);
 
@@ -24,17 +29,9 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
     .attr('cx', (d, i)=> (i+1)+20)
     .attr('cy', 25)
     .attr('r', d=>d.population/extent[1]*10)
-    const width = 700;
-    const height = 550;
-    .attr('width', width)
-    .attr('height', height);
-    const svg = d3.select('.population-plot')
-    .append('svg')
-    
-    
 
-
-  console.log('cities', data);
+    //create labels only for top 10 using opacity
+    //definitino svg text
   
 })
 
