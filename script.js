7aa783cb4abd4712cc9dd969786682a9716f7fc4
rooneyal <rooneyal@bc.edu>
2020-09-29 13:27:54 -0400

    help

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-d3.csv('cities.csv').then(data=>{
-	console.log('cities', data);
+d3.csv('cities.csv', d3.autoType).then(data=>{
+
+  //filter data to European cities only
+  data = data.filter(d=>d.eu === TRUE);
+
+  //sort the data by population size
+  data.sort((a,b)=>b.population-a.population);
+
+  //Add Labels to top 10 cities
+  //data = data.slice(0,10);
+
+  const extent = d3.extent(data, d=>d.population);
+  
+  //create simple plot
+  d3.select('body').append('svg')
+    .attr('width', 500)
+    .attr('height', 500);
+
+
+  svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', (d, i)=> (i+1)+20)
+    .attr('cy', 25)
+    .attr('r', d=>d.population/extent[1]*10)
+    const width = 700;
+    const height = 550;
+    .attr('width', width)
+    .attr('height', height);
+    const svg = d3.select('.population-plot')
+    .append('svg')
+    
+    
+
+
+  console.log('cities', data);
+  
 })
 
-d3.csv('cities.csv', d=>{
-    return {
-      ...d, // spread operator
-      eu: d.eu==='true', // convert to boolean
-      population: +d.population,
-      x: +d.x,
-      y: +d.y,
-    }
-  }).then(data=>{
-      console.log('cities', data);
-  })
